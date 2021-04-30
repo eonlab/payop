de5cdd985e735dcabc6fa2007975d0b5b4875218
eonlab <luisfsd@tecbit.eu>
2021-04-30 13:16:24 +0100

    Update PayOp.php

--- src/Constants/PayOp.php
+++ src/Constants/PayOp.php
@@ -1,12 +1,14 @@
 <?php
 namespace Eonlab\Constants;
 
+use Illuminate\Support\Facades\Config;
+
 class PayOp
 {
-    const PUBLIC_KEY = config('payop.public_key');
-    const SECRET_KEY = config('payop.secret_key');
-    const SERVICE_TOKEN = config('payop.service_token');
-    const PROJECT_ID = config('payop.project_id');
+    const PUBLIC_KEY = Config::get('payop.public_key');
+    const SECRET_KEY = Config::get('payop.secret_key');
+    const SERVICE_TOKEN = Config::get('payop.service_token');
+    const PROJECT_ID = Config::get('payop.project_id');
 
 
     const EXAMPLE_PAYER_PAN = "5555555555554444";
